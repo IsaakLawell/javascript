202eb6737158cef3f3eac1a61ef8060862a819ff
IsaakLawell <arc.demon1000@gmail.com>
2023-06-05 23:06:28 +0200

    test 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,12 @@ const Home: NextPage = () => {
         <div className="absolute h-1/2 w-screen top-1/2 left-0 bg-gradient-to-t from-blue-500 via-pink-400 to-white z-[-1] "></div>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 mx-auto mt-9">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Site <span className="text-purple-800">mauvais</span>
+            Site algo <span className="text-purple-800">mauvais</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="bite"
+              href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
